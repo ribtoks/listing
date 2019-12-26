cbe6569d7b235bbb4a45d847fa50c8cb86744046
Taras Kushnir <kutaras@outlook.com>
2019-12-25 17:44:01 -0800

    Fix go vet warnings

--- sesnotify/sesmessage.go
+++ sesnotify/sesmessage.go
@@ -1,42 +1,42 @@
 package main
 
 type SesMessage struct {
-	NotificationType string                 `json notificationType`
-	Content          string                 `json content`
-	Mail             Mail                   `json mail`
-	Bounce           Bounce                 `json bounce`
-	Complaint        Complaint              `json complaint`
-	Receipt          map[string]interface{} `json receipt`
+	NotificationType string                 `json:"notificationType"`
+	Content          string                 `json:"content"`
+	Mail             Mail                   `json:"mail"`
+	Bounce           Bounce                 `json:"bounce"`
+	Complaint        Complaint              `json:"complaint"`
+	Receipt          map[string]interface{} `json:"receipt"`
 }
 
 type Bounce struct {
-	BounceType        string          `json bounceType`
-	BounceSubType     string          `json bounceSubType`
-	BouncedRecipients []MailRecipient `json bouncedRecipients`
-	Timestamp         string          `json timestamp`
-	FeedbackId        string          `json feedbackId`
-	RemoteMtaIp       string          `json remoteMtaIp`
+	BounceType        string          `json:"bounceType"`
+	BounceSubType     string          `json:"bounceSubType"`
+	BouncedRecipients []MailRecipient `json:"bouncedRecipients"`
+	Timestamp         string          `json:"timestamp"`
+	FeedbackId        string          `json:"feedbackId"`
+	RemoteMtaIp       string          `json:"remoteMtaIp"`
 }
 
 type MailRecipient struct {
-	EmailAddress string `json emailAddress`
+	EmailAddress string `json:"emailAddress"`
 }
 
 type Complaint struct {
-	UserAgent             string          `json userAgent`
-	ComplainedRecipients  []MailRecipient `json complainedRecipients`
-	ComplaintFeedbackType string          `json complaintFeedbackType`
-	ArrivalDate           string          `json emailAddress`
-	Timestamp             string          `json emailAddress`
-	FeedbackId            string          `json emailAddress`
+	UserAgent             string          `json:"userAgent"`
+	ComplainedRecipients  []MailRecipient `json:"complainedRecipients"`
+	ComplaintFeedbackType string          `json:"complaintFeedbackType"`
+	ArrivalDate           string          `json:"emailAddress"`
+	Timestamp             string          `json:"emailAddress"`
+	FeedbackId            string          `json:"emailAddress"`
 }
 
 type Mail struct {
-	Timestamp        string                 `json timestamp`
-	Source           string                 `json source`
-	MessageId        string                 `json messageId`
-	HeadersTruncated bool                   `json headersTruncated`
-	Destination      []string               `json destination`
-	Headers          []map[string]string    `json headers`
-	CommonHeaders    map[string]interface{} `json commonHeaders`
+	Timestamp        string                 `json:"timestamp"`
+	Source           string                 `json:"source"`
+	MessageId        string                 `json:"messageId"`
+	HeadersTruncated bool                   `json:"headersTruncated"`
+	Destination      []string               `json:"destination"`
+	Headers          []map[string]string    `json:"headers"`
+	CommonHeaders    map[string]interface{} `json:"commonHeaders"`
 }
