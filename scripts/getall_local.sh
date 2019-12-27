8c7491375fcc9a39f9d3a17528df4d827ea9d8ce
Taras Kushnir <kutaras@outlook.com>
2019-12-27 07:33:37 -0800

    Remove localstack

--- scripts/getall_local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-API_TOKEN="959654580b4f0837c7f3d9201bfdd2c33941ddd7"
-
-curl -u :$API_TOKEN $LISTING_URL?newsletter=Listing2
--- scripts/subscribe_local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -v --data "newsletter=Listing2&email=foo@bar.com" "${LISTING_URL}"
-
