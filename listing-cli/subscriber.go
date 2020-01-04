1e22536eec2f87a863bdb3aadfb10410000c7902
Taras Kushnir <kutaras@outlook.com>
2020-01-04 01:45:52 +0000

    Add explicit yaml tags

--- listing-cli/subscriber.go
+++ listing-cli/subscriber.go
@@ -3,12 +3,12 @@ package main
 import "github.com/ribtoks/listing/pkg/common"
 
 type SubscriberEx struct {
-	Name         string `json:"name"`
-	Newsletter   string `json:"newsletter"`
-	Email        string `json:"email"`
-	Token        string `json:"token"`
-	Confirmed    bool   `json:"confirmed"`
-	Unsubscribed bool   `json:"unsubscribed"`
+	Name         string `json:"name" yaml:"name"`
+	Newsletter   string `json:"newsletter" yaml:"newsletter"`
+	Email        string `json:"email" yaml:"email"`
+	Token        string `json:"token" yaml:"toke"`
+	Confirmed    bool   `json:"confirmed" yaml:"confirmed"`
+	Unsubscribed bool   `json:"unsubscribed" yaml:"unsubscribed"`
 }
 
 func NewSubscriberEx(s *common.Subscriber, secret string) *SubscriberEx {
